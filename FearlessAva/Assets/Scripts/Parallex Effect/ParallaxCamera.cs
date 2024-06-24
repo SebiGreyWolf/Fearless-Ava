4f0fdf7b467f83f52f5c5ce95372e8fba1da935a
GrayWolf0 <54938154+GrayWolf0@users.noreply.github.com>
2024-06-24 19:33:36 +0200

    Parrallex Effect Test, Added a Build, Movement reword
    Added some Test sprites for parrallex effect testing
    Created a Build
    Rework movement WIP

---
+++ FearlessAva/Assets/Scripts/Parallex Effect/ParallaxCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public delegate void ParallaxCameraDelegate(float deltaMovement);
+public class ParallaxCamera : MonoBehaviour
+{
+    public ParallaxCameraDelegate onCameraTranslate;
+
+    private float oldPosition;
+
+    void Start()
+    {
+        oldPosition = transform.position.x;
+    }
+
+    void Update()
+    {
+        if (transform.position.x != oldPosition)
+        {
+            if (onCameraTranslate != null)
+            {
+                float delta = oldPosition - transform.position.x;
+                onCameraTranslate(delta);
+            }
+
+            oldPosition = transform.position.x;
+        }
+    }
+}
